2cb2bac2aca44054d3858bc09fe812624f7c7f6f
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-01 18:39:21 -0300

    se realizo el primer ejercicio

---
+++ divisionResto.py
@@ -0,0 +1,5 @@
+#Ingrese los numeros a calcular el resto
+a = int(input('\nIngrese el primer valor: '))
+b = int(input('Ingrese el segundo valor: '))
+print('\nLa division de',a,'/',b,'es',a/b)
+print('El resto de',a,'%',b,'es',a%b,'\n')
