14b43dff522089be4431b860fadbd82c0c316ed3
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-02 20:34:11 -0300

    se agrego el ejercicio 4

--- 03-areaTringulo.py
+++ 03-areaTringulo.py
@@ -5,7 +5,7 @@
 # triángulo, cargando por teclado el valor de la base, pero      #
 # sabiendo que su altura es igual al cuadrado de la base.        #
 # (Observar que la altura no es un dato... sólo se indica la     #
-# forma de calcularla de acuerdo a la base que sí es un dato).   #                                 #
+# forma de calcularla de acuerdo a la base que sí es un dato).   #                                 
 #----------------------------------------------------------------#
 
 print('\nPrograma para calcular el area del triangulo\n')
