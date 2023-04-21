9e0042703ae273561a55c0cc820e7fa77122be2e
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-20 23:29:06 -0300

    se termino el desafio 1

--- desafios/desafio1.py
+++ desafios/desafio1.py
@@ -5,7 +5,7 @@
 # la  cantidad de segundos que pasaron desde un evento dado.  El programa debe convertir esa cantidad de    #
 # segundos  a la cantidad de horas, minutos y segundos que transcurrieron. Por ejemplo, si la cantidad      #
 # de segundos  ingresada es 4452 deberá mostrar un mensaje que informe que el tiempo transcurrido fue de    #
-# 1 hora, 14 minutos  y 12 segundos. Pero la conversión solo debe mostrarse si la cantidad de horas         # 
+# 1 hora, 14 minutos  y 12 segundos. Pero la conversión solo debe mostrarse si la cantidad de horas         #
 # totales obtenida es menor o igual a 24. Si esa cantidad de horas totales es mayor a 24, el programa       #
 # debe mostrar un mensaje de la forma "Excedido". Se le pedirá comprobar su programa para cuatro            #
 # cantidades de segundos, que deberá cargar por teclado.                                                    #
@@ -17,22 +17,38 @@
 # entonces el resultado a obtener es que la cantidad total de segundos es 16568.                            #
 # ----------------------------------------------------------------------------------------------------------#
 
+# Interfaz
+print('\x1b[1;32m')
+print('---------------------------------------------------')
+print('\tCONVERSOR DE UNIDADES DE TIEMPO')
+print('---------------------------------------------------\n')
+print(' \x1b[1;36m 1) Conersion de segundos a hora:minuto:segundo')
+print(' \x1b[1;36m 2) Conersion de hora:minuto:segundo a segundos')
+print('\x1b[1;32m')
+print('---------------------------------------------------')
+print('\x1b[m')
+menu = int(input('  Ingrese una opcion: '))
 
-# Proceso normal
-num = int(input('\nIngrese la cantidad de segundos: '))
-h = int(num/3600)
-
-if h<=24:
-    m = int((num - (h*3600))/60)
-    s = num -((h*3600) + (m*60))
-    print('\n' + str(h) + ':' + str(m) + ':' + str(s) + '\n')
+if menu == 1 or menu == 2:
+    # Proceso normal
+    if menu == 1:
+        num = int(input('  Ingrese la cantidad de segundos: '))
+        h = int(num/3600)
+        if h <= 24:
+            m = int((num - (h*3600))/60)
+            s = num - ((h*3600) + (m*60))
+            print('\n  El resultado es: \x1b[1;31m' + str(h) + ':' + str(m) + ':' + str(s) + '\n')
+        else:
+            print('Excedido\n')
+    # Proceso inverso
+    if menu == 2:
+        num = input('  Ingrese con el formato 00:00:00 : ')
+        h = int(num[0] + num[1])
+        m = int(num[3] + num[4])
+        s = int(num[6] + num[7])
+        hs = int(h*3600)
+        ms = int(m*60)
+        st = hs + ms + s
+        print('\n  El resultado es: \x1b[1;31m' + str(st) + ' segundos\n')
 else:
-    print('Excedido\n')
-
-
-# Proceso inverso
-hs = int(h*3600)
-ms = int(m*60)
-st = hs + ms + s
-
-print(str(st) + '\n')
+    print('  El numero ingresado es incorrecto\n')
