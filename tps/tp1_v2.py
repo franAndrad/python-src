0023f859764701e801cb07333174fe8606cc1539
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-25 15:40:55 -0300

    se simplifico la parte de determinacion de patente para reducir el codigo en una version 2

---
+++ tps/tp1_v2.py
@@ -0,0 +1,73 @@
+# Datos ingresados
+patente = 'AA123AA'
+vehiculo = 2
+pago = 1
+pais = 1
+distancia = 10
+
+# Paices
+paises = ('Argentina', 'Bolivia', 'Brasil', 'Paraguay', 'Uruguay','Otro')
+
+# Proceso para determinar patente
+if len(patente) == 7:
+    if patente[0].isdigit() or patente[1].isdigit():
+        patente_pais = paises[5]
+    elif patente[2].isdigit() and not patente[5].isdigit() and not patente[6].isdigit():
+        patente_pais = paises[0]
+    elif patente[2].isdigit() and patente[5].isdigit() and patente[6].isdigit():
+        patente_pais = paises[1]
+    elif patente[3].isdigit() and patente[5].isdigit() and patente[6].isdigit():
+        if patente[4].isdigit():
+            patente_pais = paises[4]
+        else:
+            patente_pais = paises[2]
+    elif patente[4].isdigit() and patente[5].isdigit() and patente[6].isdigit():
+        patente_pais = paises[3]
+    else:
+        patente_pais = paises[5]
+else:
+    patente_pais = paises[5]
+
+# Cobro
+if paises[pais] == 'Brasil':
+    importe = 400
+elif paises[pais] == 'Bolivia':
+    importe = 200
+else:
+    importe = 300
+
+# Descuento por vehiculo
+if vehiculo == 0:
+    importe_vehiculo = importe - (50*importe)/100
+elif vehiculo == 1:
+    importe_vehiculo = importe
+elif vehiculo == 2:
+    importe_vehiculo = importe + (60*importe)/100
+
+# Descuento por pago de telepeage
+if pago == 1:
+    total = importe_vehiculo
+elif pago == 2:
+    total = importe_vehiculo - (10*importe_vehiculo)/100
+else:
+    total = 0
+
+# Promedio de pago por kilometro: falta redondear
+if distancia != 0:
+    promedio = round(total/distancia)
+else:
+    promedio = 'Primera vez que el vehiculo ingresa'
+
+
+print('\n')
+print('-------------------------------------------')
+print('                 TICKET                    ')
+print('-------------------------------------------')
+print(' Patente:\t\t', patente)
+print(' Procedencia:\t\t', patente_pais)
+print('-------------------------------------------')
+print(' Importe basico:\t $', importe_vehiculo)
+print(' Total:\t\t\t $', total)
+print(' Promedio/km:\t\t $', promedio)
+print('-------------------------------------------')
+print('\n')
